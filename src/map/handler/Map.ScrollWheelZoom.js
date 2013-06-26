d1461fc7469e9c661e2b57aee659fa43e74cf20b
Vladimir Agafonkin <agafonkin@gmail.com>
2013-06-26 19:30:03 -0400

    always scroll-zoom by 1 zoom, don't use wheel delta

--- src/map/handler/Map.ScrollWheelZoom.js
+++ src/map/handler/Map.ScrollWheelZoom.js
@@ -17,37 +17,20 @@ L.Map.ScrollWheelZoom = L.Handler.extend({
 	},
 
 	_onWheelScroll: function (e) {
-		var delta = L.DomEvent.getWheelDelta(e);
+		var delta = L.DomEvent.getWheelDelta(e),
+			mousePos = this._map.mouseEventToContainerPoint(e);
 
-		this._delta += delta;
-		this._lastMousePos = this._map.mouseEventToContainerPoint(e);
-
-		if (!this._startTime) {
-			this._startTime = +new Date();
-		}
-
-		var left = Math.max(40 - (+new Date() - this._startTime), 0);
-
-		clearTimeout(this._timer);
-		this._timer = setTimeout(L.bind(this._performZoom, this), left);
+		this._performZoom(delta, mousePos);
 
 		L.DomEvent.preventDefault(e);
 		L.DomEvent.stopPropagation(e);
 	},
 
-	_performZoom: function () {
+	_performZoom: function (delta, mousePos) {
 		var map = this._map,
-		    delta = this._delta,
 		    zoom = map.getZoom();
 
-		delta = delta > 0 ? Math.ceil(delta) : Math.floor(delta);
-		delta = Math.max(Math.min(delta, 4), -4);
-		delta = map._limitZoom(zoom + delta) - zoom;
-
-		this._delta = 0;
-		this._startTime = null;
-
-		if (!delta) { return; }
+		delta = delta > 0 ? 1 : -1;
 
 		map.setZoomAround(this._lastMousePos, zoom + delta);
 	}
