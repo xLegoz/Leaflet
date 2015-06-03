e503ac2af737f4e0a83d950722e90885dc8ad325
Joey Pereira <legojoey17@gmail.com>
2015-06-03 10:03:52 -0400

    v0.7.3 with beforePopupClose hook

--- dist/leaflet-src.js
+++ dist/leaflet-src.js
@@ -3910,6 +3910,12 @@ L.Popup = L.Class.extend({
 	},
 
 	onRemove: function (map) {
+		map.fire('popupbeforeclose', {popup: this});
+
+		if (this._source) {
+			this._source.fire('popupbeforeclose', {popup: this});
+		}
+
 		map._panes.popupPane.removeChild(this._container);
 
 		L.Util.falseFn(this._container.offsetWidth); // force reflow
